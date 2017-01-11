42d5425942ea5b51dabe48084fbe2153d72da9df
flamsholt <richard.flamsholt@telerik.com>
2017-01-11 01:41:32 +0100

    Report motion every 5 min, use camelCase,

--- rattrap.ino
+++ rattrap.ino
@@ -1,3 +1,4 @@
+// GPIO
 const int PIN_IN_TRAPSENSOR = D1;
 const int PIN_OUT_LIGHT     = D2;
 const int PIN_IN_IR_RIGHT   = D3;
@@ -5,32 +6,33 @@ const int PIN_IN_IR_LEFT    = D4;
 const int PIN_OUT_RELAY     = D5;
 const int PIN_IN_BUTTON     = D6;
 const int PIN_OUT_ACTLED    = D7;
+bool isButtonPressed() { return digitalRead(PIN_IN_BUTTON) == HIGH; }
+bool isTrapOpen() { return digitalRead(PIN_IN_TRAPSENSOR) == LOW; }
+void setRelay(bool on) { digitalWrite(PIN_OUT_RELAY, on ? HIGH : LOW); }
+void setLight(bool on) { digitalWrite(PIN_OUT_LIGHT, on ? HIGH : LOW); }
+void setActLed(bool on) { digitalWrite(PIN_OUT_ACTLED, on ? HIGH : LOW); }
 
-bool IsButtonPressed() { return digitalRead(PIN_IN_BUTTON) == HIGH; }
-bool IsTrapOpen() { return digitalRead(PIN_IN_TRAPSENSOR) == LOW; }
-void SetRelay(bool on) { digitalWrite(PIN_OUT_RELAY, on ? HIGH : LOW); }
-void SetLight(bool on) { digitalWrite(PIN_OUT_LIGHT, on ? HIGH : LOW); }
-void SetActLed(bool on) { digitalWrite(PIN_OUT_ACTLED, on ? HIGH : LOW); }
-
-volatile bool IndicateOpen = false;
-volatile bool IndicateIrLeft = false;
-volatile bool IndicateIrRight = false;
-int IsOpen = false;
-
-int OnCommandOpen(String command)
+// Trap door
+int isOpen = false;
+volatile bool indicateOpen = false;
+int onCommandOpen(String command)
 {
-  IndicateOpen = true;
-  return 1;
+  indicateOpen = true;
+  return 0;
 }
 
-void OnIrLeftChange()
+// Motion IR detectors
+const int MOTIONPUBLISH_INTERVAL_MSEC = 5 * 60 * 1000; // Every 5 min
+volatile int motionCount = 0;
+void onPublishMotion()
 {
-  IndicateIrLeft = true;
+  Particle.publish("motion", String::format("%d", motionCount / 2));
+  motionCount = 0;
 }
-
-void OnIrRightChange()
+Timer motionPublishTimer(MOTIONPUBLISH_INTERVAL_MSEC, onPublishMotion);
+void onMotionDetected()
 {
-  IndicateIrRight = true;
+  motionCount++;
 }
 
 // This routine runs only once upon reset
@@ -46,61 +48,38 @@ void setup()
   pinMode(PIN_OUT_RELAY, OUTPUT);
   pinMode(PIN_OUT_ACTLED, OUTPUT);
 
-  attachInterrupt(PIN_IN_IR_LEFT, OnIrLeftChange, FALLING);
-  attachInterrupt(PIN_IN_IR_RIGHT, OnIrRightChange, FALLING);
+  attachInterrupt(PIN_IN_IR_LEFT, onMotionDetected, FALLING);
+  attachInterrupt(PIN_IN_IR_RIGHT, onMotionDetected, FALLING);
 
-  SetLight(false);
-  SetRelay(false);
+  setLight(false);
+  setRelay(false);
 
-  Particle.variable("is_open", IsOpen);
-  Particle.function("open", OnCommandOpen);
+  Particle.variable("is_open", isOpen);
+  Particle.function("open", onCommandOpen);
+  motionPublishTimer.start();
 }
 
 // This routine gets called repeatedly, like once every 5-15 milliseconds.
 void loop()
 {
   // Update open-state and set light on if trap is open
-  if (IsTrapOpen() != IsOpen)
+  if (isOpen != isTrapOpen())
   {
-    IsOpen = IsTrapOpen();
-    SetLight(IsOpen);
+    isOpen = isTrapOpen();
+    setLight(isOpen);
   }
 
-  // Submit IR events, unless blocked by trap-door; if so then clear them
-  if (!IsOpen)
+  // If not already open then open trap if so requested
+  if (!isOpen && (indicateOpen || isButtonPressed()))
   {
-    if (IndicateIrLeft)
-    {
-      IndicateIrLeft = false;
-      Particle.publish("ir_left");
-    }
-    if (IndicateIrRight)
-    {
-      IndicateIrRight = false;
-      Particle.publish("ir_right");
-    }
-    if (IsButtonPressed())
-    {
-      IndicateOpen = true;
-    }
-  }
-  else
-  {
-    IndicateIrLeft = false;
-    IndicateIrRight = false;
-  }
-
-  // Open trap if so requested and it's not already open
-  if (IndicateOpen && !IsOpen)
-  {
-    SetRelay(true);
+    setRelay(true);
     int timeout = Time.now() + 10; // Wait max n seconds for trap to open
-    while (!IsTrapOpen() && Time.now() < timeout)
+    while (!isTrapOpen() && Time.now() < timeout)
     {
       delay(100);
     }
-    delay(400); // keep magnet turned on a bit more
-    SetRelay(false);
-    IndicateOpen = false;
+    delay(500); // keep magnet turned on a bit more
+    setRelay(false);
   }
+  indicateOpen = false;
 }
