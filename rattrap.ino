186e32e4909313d45c687aee8b4e5c91d2c447b6
flamsholt <richard.flamsholt@telerik.com>
2017-02-01 01:36:51 +0100

    Fix motion-count and Publish directly in loop

--- rattrap.ino
+++ rattrap.ino
@@ -8,6 +8,8 @@ const int PIN_IN_BUTTON     = D6;
 const int PIN_OUT_ACTLED    = D7;
 bool isButtonPressed() { return digitalRead(PIN_IN_BUTTON) == HIGH; }
 bool isTrapOpen() { return digitalRead(PIN_IN_TRAPSENSOR) == LOW; }
+bool isIrRight() { return digitalRead(PIN_IN_IR_RIGHT) == LOW; }
+bool isIrLeft() { return digitalRead(PIN_IN_IR_LEFT) == LOW; }
 void setRelay(bool on) { digitalWrite(PIN_OUT_RELAY, on ? HIGH : LOW); }
 void setLight(bool on) { digitalWrite(PIN_OUT_LIGHT, on ? HIGH : LOW); }
 void setActLed(bool on) { digitalWrite(PIN_OUT_ACTLED, on ? HIGH : LOW); }
@@ -22,18 +24,10 @@ int onCommandOpen(String command)
 }
 
 // Motion IR detectors
-const int MOTIONPUBLISH_INTERVAL_MSEC = 5 * 60 * 1000; // Every 5 min
-volatile int motionCount = 0;
-void onPublishMotion()
-{
-  Particle.publish("motion", String::format("%d", motionCount / 2));
-  motionCount = 0;
-}
-Timer motionPublishTimer(MOTIONPUBLISH_INTERVAL_MSEC, onPublishMotion);
-void onMotionDetected()
-{
-  motionCount++;
-}
+int motionCount = 0;
+bool motionDetect = false;
+long motionDetectEndTimeInMsec = 0;
+int motionCountEndTimeInSec = 0;
 
 // This routine runs only once upon reset
 void setup()
@@ -41,22 +35,18 @@ void setup()
   Serial.begin(9600);
 
   pinMode(PIN_IN_TRAPSENSOR, INPUT_PULLDOWN);
-  pinMode(PIN_IN_IR_LEFT, INPUT_PULLDOWN);
-  pinMode(PIN_IN_IR_RIGHT, INPUT_PULLDOWN);
+  pinMode(PIN_IN_IR_LEFT, INPUT_PULLUP);
+  pinMode(PIN_IN_IR_RIGHT, INPUT_PULLUP);
   pinMode(PIN_IN_BUTTON, INPUT_PULLDOWN);
   pinMode(PIN_OUT_LIGHT, OUTPUT);
   pinMode(PIN_OUT_RELAY, OUTPUT);
   pinMode(PIN_OUT_ACTLED, OUTPUT);
-
-  attachInterrupt(PIN_IN_IR_LEFT, onMotionDetected, FALLING);
-  attachInterrupt(PIN_IN_IR_RIGHT, onMotionDetected, FALLING);
 
   setLight(false);
   setRelay(false);
 
   Particle.variable("is_open", isOpen);
   Particle.function("open", onCommandOpen);
-  motionPublishTimer.start();
 }
 
 // This routine gets called repeatedly, like once every 5-15 milliseconds.
@@ -73,7 +63,7 @@ void loop()
   if (!isOpen && (indicateOpen || isButtonPressed()))
   {
     setRelay(true);
-    int timeout = Time.now() + 10; // Wait max n seconds for trap to open
+    auto timeout = Time.now() + 10; // Wait max 10 seconds for trap to open
     while (!isTrapOpen() && Time.now() < timeout)
     {
       delay(100);
@@ -82,4 +72,31 @@ void loop()
     setRelay(false);
   }
   indicateOpen = false;
+
+  // Update motion detected. Don't detect changes during the debounce
+  // period and only detect actual changes and report on motion detected.
+  if (millis() > motionDetectEndTimeInMsec)
+  {
+    auto motion = isIrLeft() || isIrRight();
+    if (motion != motionDetect)
+    {
+      motionDetect = motion;
+      motionDetectEndTimeInMsec = millis() + 200; // debounce readings
+      if (motionDetect) // only report motion-detect, not motion-undetect
+      {
+        motionCount++;
+      }
+    }
+  }
+
+  // Submit motions at regular intervals, if there are any
+  if (Time.now() >= motionCountEndTimeInSec)
+  {
+    if (motionCount > 0)
+    {
+      Particle.publish("motion", String::format("%d", motionCount));
+      motionCount = 0;
+    }
+    motionCountEndTimeInSec = Time.now() + 5*60; // repeat in 5 minutes
+  }
 }
